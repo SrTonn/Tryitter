d7c924f919cd0620750ee12be98a081c3eb31912
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-12-08 15:21:12 -0300

    add migrations

---
+++ Tryitter/Migrations/20221208174448_fillUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tryitter.Migrations
+{
+    public partial class fillUser : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Users(Name, Email, Password) Values('Urbe', 'Urbe88@email.com', 'UrbeRock')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Users");
+        }
+    }
+}
---
+++ Tryitter/Migrations/20221208175300_fillPost.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tryitter.Migrations
+{
+    public partial class fillPost : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Posts(Title, Description, ImageUrl, DataPost, UserId)" +
+                "Values('CSharp Null-Coalescing Operator', " +
+                "'The other day I was working in Markdown Monster - a WPF application - where I needed to display a list of matches from a directory search. The search supports searching the file content as well as file names, so the results would reflect how many search phrase matches there are for each matched file. But I wanted to display files that have 0 matches - ie. where only the file name matches but not any content - as an empty string rather than displaying 0.'," +
+                "'https://i.pinimg.com/736x/03/1b/68/031b68882265722dede1080a200f015a.jpg'," + 
+                "now()," +
+                "1)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Posts");
+        }
+    }
+}

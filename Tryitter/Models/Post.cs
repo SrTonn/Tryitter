1ae28756af6dc1db992e0b3f8ddf74d9d1af1bf2
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-12-08 15:21:16 -0300

    add models

---
+++ Tryitter/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Tryitter.Models;
+
+public class Post
+{
+    [Key]
+    public int PostId { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string? Title { get; set; }
+    [MaxLength(500)]
+    public string? Description { get; set; }
+    [Required]
+    [MaxLength(300)]
+    public string? ImageUrl { get; set; }
+
+    public DateTime DataPost { get; set; }
+    public int UserId { get; set; }
+
+    [JsonIgnore]
+    public User? User { get; set; }
+}
---
+++ Tryitter/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tryitter.Models;
+
+public class User
+{
+    [Key] //Optional
+    public int UserId { get; set; }
+    [Required]
+    [MinLength(3), MaxLength(10)]
+    public string? Name { get; set; }
+    [Required]
+    [MinLength(3), MaxLength(30)]
+    public string? Email { get; set; }
+    [Required]
+    [MinLength(6), MaxLength(20)]
+    public string? Password { get; set; }
+    public IEnumerable<Post> Posts { get; set; }
+
+    public User()
+    {
+        Posts = new List<Post>();
+    }
+}

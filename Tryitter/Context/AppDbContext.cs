7ba4a3ef14d8284bf07bda7366e52118736543f7
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-12-08 15:21:22 -0300

    add context

---
+++ Tryitter/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tryitter.Models;
+
+namespace Tryitter.Context;
+
+public class AppDbContext : DbContext
+{
+	public DbSet<Post>? Posts { get; set; }
+	public DbSet<User>? Users { get; set; }
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}

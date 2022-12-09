d3295deeb71436e2d4d8ca1b52d6a66526ef2c98
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-12-09 15:44:19 -0300

    add controllers

---
+++ Tryitter/Controllers/PostsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tryitter.Context;
+using Tryitter.Models;
+using Tryitter.Validations;
+
+namespace Tryitter.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PostsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Post>> GetAll()
+        {
+            var posts = _context.Posts;
+            if (posts is null)
+                return NotFound("Posts não encontrados...");
+
+            return Ok(posts);
+        }
+
+        [HttpGet("{id:int}", Name = "GetPost")]
+        public ActionResult<IEnumerable<Post>> GetById(int id)
+        {
+            var post = _context.Posts!.FirstOrDefault(post => post.PostId == id);
+            if(post is null)
+                return NotFound("Post não encontrado.");
+
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Post post)
+        {
+            if (post is null)
+                return BadRequest();
+
+            if (!UserValidation.IsValid(_context.GetUser(post.UserId)))
+                return NotFound("Usuário não encontrado.");
+
+            _context.Posts!.Add(post);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetPost",
+                new { id = post.PostId }, post);
+        }
+
+        [HttpPut]
+        public ActionResult Put(int id, Post post)
+        {
+            if (id != post.PostId)
+                return BadRequest();
+
+            _context.Entry(post).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(post);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var post = _context.Posts!.FirstOrDefault(post => post.PostId == id);
+
+            if (post is null)
+                return NotFound("Usuário não localizado.");
+
+            _context.Posts!.Remove(post);
+            _context.SaveChanges();
+
+            return Ok(post);
+        }
+    }
+}
---
+++ Tryitter/Controllers/UsersController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tryitter.Context;
+using Tryitter.Models;
+using Tryitter.Validations;
+
+namespace Tryitter.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context; 
+
+        public UsersController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAll()
+        {
+            var users = _context.Users;
+            if(users is null)
+                return NotFound("Usuários não encontrados...");
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}", Name="GetUser")]
+        public ActionResult<User> Get(int id)
+        {
+            var user = _context.GetUser(id);
+            if (UserValidation.IsValid(user))
+                return Ok(user);
+
+            return NotFound("Usuário não encontrado.");
+        }
+
+        [HttpPost]
+        public ActionResult Post(User user)
+        {
+            if (user is null)
+                return BadRequest();
+
+            _context.Users!.Add(user);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetUser",
+                new { id = user.UserId }, user);
+        }
+
+        [HttpPut]
+        public ActionResult Put(int id, User user)
+        {
+            if (id != user.UserId)
+                return BadRequest();
+
+            _context.Entry(user).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(user);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var user = _context.Users!.FirstOrDefault(user => user.UserId == id);
+
+            if (user is null)
+                return NotFound("Usuário não localizado.");
+
+            _context.Users!.Remove(user);
+            _context.SaveChanges();
+
+            return Ok(user);
+        }
+    }
+}

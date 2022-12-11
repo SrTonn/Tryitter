25f6556cfc8bc77c1aafca9b6897a292ecd085bd
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-12-11 16:42:32 -0300

    add DTOs

---
+++ Tryitter/DTOs/PostDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Tryitter.DTOs
+{
+    public class PostDTO
+    {
+        public int PostId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Tryitter/DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Tryitter.DTOs
+{
+    public class UserDTO
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public bool Admin { get; set; }
+    }
+}

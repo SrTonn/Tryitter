b9a2ff4e04a1f2009a042a3c92623090bdeb17ab
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-12-12 12:58:22 -0300

    adição de métodos para leitura de token

--- Tryitter/Context/AppDbContext.cs
+++ Tryitter/Context/AppDbContext.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Mvc;
 using Tryitter.Models;
 
 namespace Tryitter.Context;
@@ -12,5 +15,33 @@ public class AppDbContext : DbContext
     public User? GetUser(int id)
     {
         return Users!.AsNoTracking().FirstOrDefault(user => user.UserId == id);
+    }
+    public User? GetUser(string email)
+    {
+        return Users!.AsNoTracking().FirstOrDefault(user => user.Email == email);
+    }    
+    public User? GetUser(StringValues token)
+    {
+
+        var claims = GetTokenClaims(token);
+        var email = claims.Claims.First().Value;
+
+        return Users!.AsNoTracking().FirstOrDefault(user => user.Email == email);
+    }
+
+    public JwtSecurityToken GetTokenClaims(StringValues token)
+    {
+        var jwt = token.ToString();
+
+        if (jwt.Contains("Bearer"))
+        {
+            jwt = jwt.Replace("Bearer", "").Trim();
+        }
+
+        var handler = new JwtSecurityTokenHandler();
+
+        var finalToken = handler.ReadJwtToken(jwt);
+
+        return finalToken;
     }
 }

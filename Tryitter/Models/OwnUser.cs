eafa811db0bea5a5759f1e680b462a42c9eb1d18
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-12-12 12:59:27 -0300

    add model

---
+++ Tryitter/Models/OwnUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Tryitter.Models;
+//[Index(nameof(Email), IsUnique = true)]
+public class OwnUser
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+}

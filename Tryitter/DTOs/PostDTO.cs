4886ecab61c707bc1a5f7cb5c05baeb562b53818
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-12-14 15:41:23 -0300

    alteracao nos DTOs

--- Tryitter/DTOs/PostDTO.cs
+++ Tryitter/DTOs/PostDTO.cs
@@ -2,10 +2,8 @@
 {
     public class PostDTO
     {
-        public int PostId { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
-        public int UserId { get; set; }
     }
 }
